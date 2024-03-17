cdcdb7befa0af932188877c80661c498bba83f00
KaleIsProgramming <95875856+KaleIsProgramming@users.noreply.github.com>
2024-03-17 15:46:35 +0100

    Feat: Made Search Bar
    Feat: Made search bar that filter projects by their name

--- front-end/src/components/ProjectsList.tsx
+++ front-end/src/components/ProjectsList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import { Project } from "./";
 
@@ -15,7 +16,17 @@ export const ProjectsList = () => {
             image: './',
         },
         {
-            name: 'ToDoAPP',
+            name: 'Inna',
+            description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
+            image: './',
+        },
+        {
+            name: 'ReadySteady',
+            description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
+            image: './',
+        },
+        {
+            name: 'Kocham koty',
             description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
             image: './',
         },
@@ -39,24 +50,37 @@ export const ProjectsList = () => {
             description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
             image: './',
         },
-        {
-            name: 'ToDoAPP',
-            description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
-            image: './',
-        },
-        {
-            name: 'ToDoAPP',
-            description: 'ToDoApp that works with selfmade REST API and have alot of functionality',
-            image: './',
-        },
-    ]
+    ];
+
+    const [ filterValue, setFilterValue ] = useState('');
+    console.log(filterValue)
 
     return(
-        <StyledProjectsList>
-            {projects.map((item, i) => {
-                return <Project key={i} data={item}/>
-            })}
-        </StyledProjectsList>
+    <>
+        <Header>
+            Here u can check my creations
+            <Filter>
+                <input onChange={(e:any) => setFilterValue(e.target.value) } type="text" placeholder="search..." />
+            </Filter>
+        </Header>
+        {
+        filterValue === '' ? 
+
+            <StyledProjectsList>
+                {projects.map((item, i) => {
+                    return <Project key={i} data={item}/>
+                })}
+            </StyledProjectsList>
+
+        : 
+            <StyledProjectsList>
+                {projects.filter(item => item.name.toLocaleLowerCase().includes(filterValue.toLocaleLowerCase())).map((item, i) => {
+                    return <Project key={i} data={item}/>
+                })}
+            </StyledProjectsList>
+        }
+
+    </>
     )
 }
 
@@ -69,3 +93,33 @@ const StyledProjectsList = styled.div`
     display: flex;
     flex-wrap: wrap;
 `;
+
+const Header = styled.h1`
+    position: relative;
+    height: 50px;
+    width: 80%;
+    text-align: center;
+    color: white;
+`;
+
+const Filter = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    height: 100%;
+    width: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+        height: 32px;
+        width: 154px;
+        border-radius: 10px;
+        color: white;
+        border: none;
+        background: rgb(70, 65, 65);
+        padding: 8px;
+    }
+`;
+
